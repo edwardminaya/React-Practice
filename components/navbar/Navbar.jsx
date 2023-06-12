75faa23386d1174fce64b5af58d66a27bad190bf
“Edward <eminaya49@gmail.com>
2023-06-12 13:26:43 -0500

    created assets folder for images and imported to navbar logo.svg

--- components/navbar/Navbar.jsx
+++ components/navbar/Navbar.jsx
@@ -1,10 +1,16 @@
 import React from "react";
+import { RiMenu3Line, RiCloseLin } from "react-icons/ri";
+import logo from "../../assets/logo.svg";
 import "./navbar.css";
 
 const Navbar = () => {
   return (
-    <div>
-      <h1>Navbar</h1>
+    <div className="gpt3__navbar">
+      <div className="gpt3__navbar-links">
+        <div className="gpt3__navbar-links_logo">
+          <img src={logo} alt="logo" />
+        </div>
+      </div>
     </div>
   );
 };
